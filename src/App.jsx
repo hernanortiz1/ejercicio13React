b978ab6a772ac48f8b21b0aa0f98e9e23c040fd2
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-29 20:55:42 -0300

    baje footer y cambie color de fondo en main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-
+import Formulario from "./components/Formulario";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-
-  return (
+  return(
     <>
-     
+      <main className="bg-primary-subtle">
+        <h1 className="my-3 text-center fw-bold shadow py-3 bg-white">
+          Clima
+        </h1>
+        <div>
+          <Formulario />
+        </div>
+      </main>
+      <footer className="bg-dark text-light text-center py-3">
+        &copy; Todos los derechos reservados
+      </footer>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+
+
+const Formulario = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Formulario;
