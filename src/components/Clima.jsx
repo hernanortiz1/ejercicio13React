da07a06a4013f46b500f372327e7e55559584472
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-29 23:01:35 -0300

    agregue clase para icono en hoja de estilo y modifique funcion obtenerClima

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -1,21 +1,48 @@
 import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
 
-const Clima = () => {
+const Clima = ({ climaProps }) => {
   return (
     <div>
-      <div className="text-center bg-white rounded-3 p-3 container ">
-        <Row className="mb-3 ">
-          <Form.Group as={Col} md="6" className="d-flex flex-column">
-            <Form.Label>Pais, ciudad</Form.Label>
-            <img src="" alt="Icono clima" />
+      <div className="text-center bg-white rounded-3 p-3 container">
+        <Row className="m-3 ">
+          <Form.Group as={Col} md="6" className="d-flex flex-column shadow rounded-3 py-3">
+            <Form.Label>
+              Pais: {climaProps.sys?.country || "No encontrado"}, Ciudad:{" "}
+              {climaProps.name || "No encontrado"}
+            </Form.Label>
+            <div className="d-flex justify-content-center align-items-center">
+              <img
+                src={
+                  climaProps.weather?.[0]?.icon
+                    ? `https://openweathermap.org/img/wn/${climaProps.weather[0].icon}@2x.png`
+                    : ""
+                }
+                alt="Icono clima"
+                className="iconoClima"
+              />
+            </div>
+
+            <Form.Label className="text-capitalize">
+              {climaProps.weather?.[0]?.description || "Icono clima"}
+            </Form.Label>
           </Form.Group>
-          <Form.Group as={Col} md="6">
-            <div className="d-flex flex-column text-start">
-              <Form.Label>Temperatura</Form.Label>
-              <Form.Label>Sensación térmica</Form.Label>
-              <Form.Label>Algo de nubes</Form.Label>
-              <Form.Label>Humedad</Form.Label>
-              <Form.Label>nubosidad</Form.Label>
+          <Form.Group as={Col} md="6" className="py-3">
+            <div className="d-flex flex-column text-start  shadow rounded-3 p-3 bg-secondary-subtle">
+              <Form.Label className="fw-bold fs-5">
+                Temperatura: {climaProps.main?.temp || "No encontrado"}
+              </Form.Label>
+              <Form.Label className="fw-bold fs-5">
+                Sensación térmica: {climaProps.main?.feels_like || "No encontrado"} °C
+              </Form.Label>
+              <Form.Label className="fw-bold fs-5">
+                Humedad: {climaProps.main?.humidity || "No encontrado"} %
+              </Form.Label>
+              <Form.Label className="fw-bold fs-5">
+                Nubosidad: {climaProps.clouds?.all || "No encontrado"} %
+              </Form.Label>
+              <Form.Label className="fw-bold fs-5">
+                Viento: {climaProps.wind?.speed || "No encontrado"} m/s
+              </Form.Label>
             </div>
           </Form.Group>
         </Row>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,21 +1,21 @@
 import { Button, Col, Form, Row } from "react-bootstrap";
 import Clima from "./Clima";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import RingLoader from "react-spinners/RingLoader";
 import Swal from "sweetalert2";
 
 const Formulario = () => {
   const [pais, setPais] = useState("");
   const [ciudad, setCiudad] = useState("");
-  const [clima, setClima] = useState([]);
+  const [clima, setClima] = useState(null);
   const [mostrarSpinner, setMostrarSpinner] = useState(false);
 
   const obtenerClima = async () => {
     if (pais === "" || ciudad === "") {
       Swal.fire({
         icon: "error",
-        title: "Ingrese Pais y ciudad",
-        text: "Ingrese Pais y ciudad, por último consultar",
+        title: "Ciudad o pais no encontrado",
+        text: "Verifica el nombre de la ciudad y el país e intenta nuevamente",
       });
       return;
     }
@@ -25,13 +25,20 @@ const Formulario = () => {
       const respuesta = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=026b51c14f1835bd462133b357c41c2f&units=metric&lang=es`
       );
+      const datos = await respuesta.json();
+      console.log(datos);
 
-      if (respuesta.status === 200) {
-        const datos = await respuesta.json();
-        console.log(datos.results);
-        setClima(datos.results);
+      if (datos.cod === "404") {
+        Swal.fire({
+          icon: "error",
+          title: "Ciudad o país no encontrado",
+          text: "Verifica el nombre de la ciudad y el país e intenta nuevamente",
+        });
         setMostrarSpinner(false);
+        return;
       }
+       setClima(datos);
+      setMostrarSpinner(false);
     } catch (error) {
       console.error(error);
     }
@@ -41,17 +48,17 @@ const Formulario = () => {
     <div>
       <section className="p-3 border rounded-3 bg-white container">
         <Form.Group>
-          <div className="d-md-flex align-items-center gap-2 my-2">
+          <div className="d-md-flex align-items-center gap-2 my-2 text-center">
             <Form.Label className="fs-5 fw-bold">Pais</Form.Label>
             <Form.Control
               type="text"
               placeholder="Ingrese pais"
               required
               value={pais}
-              onChange={(e) => setPais(e.target.value)}
+              onChange={(e) => setPais(e.target.value.toUpperCase())}
             />
 
-            <Form.Label className="fs-5 fw-bold ms-md-4">Ciudad</Form.Label>
+            <Form.Label className="fs-5 fw-bold ms-md-4 mt-3 mt-md-0">Ciudad</Form.Label>
             <Form.Control
               type="text"
               placeholder="Ingrese ciudad"
@@ -77,7 +84,7 @@ const Formulario = () => {
           <div className="my-4 d-flex justify-content-center align-items-center">
             <RingLoader color="#0d6efd" loading={mostrarSpinner} size={100} />
           </div>
-        ) : clima.length === 0 ? (
+        ) : clima === null? (
           <p className="text-center fs-3">No hay datos para mostrar</p>
         ) : (
           <Clima climaProps={clima} />
