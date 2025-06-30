6d7874fcd6a36af0415359e087b3d9f1de42beda
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-29 21:22:02 -0300

    agregue icono de clima en titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Formulario from "./components/Formulario";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../src/img/clima.png"
 
 function App() {
   return(
     <>
       <main className="bg-primary-subtle">
-        <h1 className="my-3 text-center fw-bold shadow py-3 bg-white">
+        <div className="my-3 text-center fw-bold shadow py-3 bg-white d-flex justify-content-center align-items-center">
+          <h1 className="me-3">
           Clima
         </h1>
+        <img src={logo} alt="logo del clima" className="imgLogo"/>
+        </div>
+        
         <div>
           <Formulario />
         </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,27 @@
-
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 const Formulario = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <section className="p-3 border rounded-3 bg-white container">
+        <Form.Group>
+          
+          <div className="d-md-flex align-items-center gap-2 my-2">
+            <Form.Label className="fs-5 fw-bold">Pais</Form.Label>
+            <Form.Control required type="text" placeholder="Ingrese pais" />
+
+            <Form.Label className="fs-5 fw-bold ms-md-4">Ciudad</Form.Label>
+            <Form.Control required type="text" placeholder="Ingrese ciudad" />
+            <div className="mt-3 mt-md-0 text-center">
+              <Button>Consultar</Button>
+            </div>
+          </div>
+        </Form.Group>
+      </section>
+
+      <section className="mt-4"></section>
+    </div>
+  );
 };
 
 export default Formulario;
